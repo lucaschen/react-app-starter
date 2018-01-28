2af25beaadaa660cc472827634a4ef63e17b6ba5
Lucas Chen <2109121+parkroolucas@users.noreply.github.com>
2018-01-28 20:30:20 +1100

    Initial commit; set up basic working boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const includePaths = [path.resolve(__dirname, "./src")];
+
+module.exports = {
+  devServer: {
+    contentBase: "./dist",
+    historyApiFallback: true,
+    hot: true,
+    watchContentBase: true
+  },
+  entry: path.resolve(__dirname, "./src/App.js"),
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: includePaths,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        include: includePaths,
+        use: [
+          "style-loader",
+          "css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
